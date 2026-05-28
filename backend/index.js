174258f1c77ff917d2fc6386fda51bbc26f357a9
yash <yashgarg2810@gmail.com>
2026-05-28 16:23:26 +0530

    Fix render port

--- backend/index.js
+++ backend/index.js
@@ -43,9 +43,11 @@ app.post('/register', async (req, res) => {
   }
 })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000')
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
 // LOGIN
